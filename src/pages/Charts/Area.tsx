81eabbaa823e9c6402cda18e6738bcb3c69f01dc
Nguyen Duong <muoi07052001@gmail.com>
2022-07-08 09:05:11 +0700

    Add Line + Area Chart pages

--- src/pages/Charts/Area.tsx
+++ src/pages/Charts/Area.tsx
@@ -1,7 +1,50 @@
 import React from 'react';
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  DateTime,
+  Legend,
+  SplineAreaSeries,
+  AxisModel,
+} from '@syncfusion/ej2-react-charts';
+import { Header } from '../../components';
+import {
+  StateContextType,
+  useStateContext,
+} from '../../contexts/ContextProvider';
+
+import {
+  areaCustomSeries,
+  areaPrimaryYAxis,
+  areaPrimaryXAxis,
+} from '../../data/dummy';
 
 const Area: React.FC = () => {
-  return <div>Area</div>;
+  const { currentMode } = useStateContext() as StateContextType;
+
+  return (
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category='Area' title="Inflation Rate in Percentage" />
+    <ChartComponent
+      id="area-chart"
+      height="420px"
+      primaryXAxis={areaPrimaryXAxis as AxisModel}
+      primaryYAxis={areaPrimaryYAxis as AxisModel}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      background={currentMode === 'Dark' ? '#33373E' : '#FFF'}
+    >
+      <Inject services={[SplineAreaSeries, DateTime, Legend]} />
+      <SeriesCollectionDirective>
+        {areaCustomSeries.map((item, index) => (
+          <SeriesDirective key={index} {...item} />
+        ))}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+    </div>
+  );
 };
 
 export default Area;
