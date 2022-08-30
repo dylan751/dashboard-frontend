9fbaad87183a063bba464c3255e63154bc188647
Nguyen Duong <muoi07052001@gmail.com>
2022-08-30 10:32:14 +0700

    Add charts

--- src/pages/Charts/ColorMapping.tsx
+++ src/pages/Charts/ColorMapping.tsx
@@ -1,7 +1,72 @@
 import React from 'react';
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  ColumnSeries,
+  Category,
+  Tooltip,
+  Legend,
+  RangeColorSettingsDirective,
+  RangeColorSettingDirective,
+  AxisModel,
+} from '@syncfusion/ej2-react-charts';
 
-const ColorMapping: React.FC = () => {
-  return <div>ColorMapping</div>;
+import {
+  colorMappingData,
+  ColorMappingPrimaryXAxis,
+  ColorMappingPrimaryYAxis,
+  rangeColorMapping,
+} from '../../data/dummy';
+import { ChartsHeader } from '../../components';
+import {
+  StateContextType,
+  useStateContext,
+} from '../../contexts/ContextProvider';
+
+const ColorMapping = () => {
+  const { currentMode } = useStateContext() as StateContextType;
+
+  return (
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <ChartsHeader
+        category="Color Mappping"
+        title="USA CLIMATE - WEATHER BY MONTH"
+      />
+      <div className="w-full">
+        <ChartComponent
+          id="charts"
+          primaryXAxis={ColorMappingPrimaryXAxis as AxisModel}
+          primaryYAxis={ColorMappingPrimaryYAxis as AxisModel}
+          chartArea={{ border: { width: 0 } }}
+          legendSettings={{ mode: 'Range', background: 'white' }}
+          tooltip={{ enable: true }}
+          background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+        >
+          <Inject services={[ColumnSeries, Tooltip, Category, Legend]} />
+          <SeriesCollectionDirective>
+            <SeriesDirective
+              dataSource={colorMappingData[0]}
+              name="USA"
+              xName="x"
+              yName="y"
+              type="Column"
+              cornerRadius={{
+                topLeft: 10,
+                topRight: 10,
+              }}
+            />
+          </SeriesCollectionDirective>
+          <RangeColorSettingsDirective>
+            {rangeColorMapping.map((item, index) => (
+              <RangeColorSettingDirective key={index} {...item as any} />
+            ))}
+          </RangeColorSettingsDirective>
+        </ChartComponent>
+      </div>
+    </div>
+  );
 };
 
 export default ColorMapping;
--- src/pages/Charts/Financial.tsx
+++ src/pages/Charts/Financial.tsx
@@ -1,7 +1,79 @@
 import React from 'react';
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  HiloSeries,
+  Tooltip,
+  DateTime,
+  Zoom,
+  Logarithmic,
+  Crosshair,
+  AxisModel,
+} from '@syncfusion/ej2-react-charts';
 
-const Financial: React.FC = () => {
-  return <div>Financial</div>;
+import {
+  financialChartData,
+  FinancialPrimaryXAxis,
+  FinancialPrimaryYAxis,
+} from '../../data/dummy';
+import {
+  StateContextType,
+  useStateContext,
+} from '../../contexts/ContextProvider';
+import { ChartsHeader } from '../../components';
+
+const date1 = new Date('2017, 1, 1');
+
+function filterValue(value) {
+  if (value.x >= date1) {
+    return value.x, value.high, value.low;
+  }
+}
+const returnValue = financialChartData.filter(filterValue);
+
+const Financial = () => {
+  const { currentMode } = useStateContext() as StateContextType;
+
+  return (
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <ChartsHeader category="Financial" title="AAPLE Historical" />
+      <div className="w-full">
+        <ChartComponent
+          id="charts"
+          primaryXAxis={FinancialPrimaryXAxis as AxisModel}
+          primaryYAxis={FinancialPrimaryYAxis as AxisModel}
+          chartArea={{ border: { width: 0 } }}
+          tooltip={{ enable: true, shared: true }}
+          crosshair={{ enable: true, lineType: 'Vertical', line: { width: 0 } }}
+          background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+        >
+          <Inject
+            services={[
+              HiloSeries,
+              Tooltip,
+              DateTime,
+              Logarithmic,
+              Crosshair,
+              Zoom,
+            ]}
+          />
+          <SeriesCollectionDirective>
+            <SeriesDirective
+              dataSource={returnValue}
+              xName="x"
+              yName="low"
+              name="Apple Inc"
+              type="Hilo"
+              low="low"
+              high="high"
+            />
+          </SeriesCollectionDirective>
+        </ChartComponent>
+      </div>
+    </div>
+  );
 };
 
 export default Financial;
